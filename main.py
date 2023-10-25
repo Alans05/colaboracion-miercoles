7e61e85c89f09b7addccd0bff205e3e917b24d7f
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-10-25 17:16:46 -0500

    no hice el trabajo

---
+++ main.py
@@ -0,0 +1,19 @@
+# elabore un algoritmo que lea dos notas, les obtenga el promedio y si la nota es mayor o igual a 3.0 inidque si gano o si es mejor a 3.0 indique que perdio
+
+# Inicio
+
+# Declaracion de variables
+nota1 = 0.0
+nota2 = 0.0
+promedio = 0.0
+
+# Entrada de datos
+nota1 = float(input("Ingrese la primera nota: "))
+nota2 = float(input("Ingrese la segunda nota: "))
+promedio = (nota1 + nota2) / 2
+
+# Proceso
+if promedio >= 3.0:
+    print("Gano")
+else:
+    print("Perdio")
